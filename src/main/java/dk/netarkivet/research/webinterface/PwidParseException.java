a5dcdeabcae2c09520fbc70d42a4a0078f899aeb
Søren Vejrup Carlsen <svc@kb.dk>
2017-10-23 16:17:26 +0200

    Finished a PWID parser/validator

---
+++ src/main/java/dk/netarkivet/research/webinterface/PwidParseException.java
@@ -0,0 +1,9 @@
+package dk.netarkivet.research.webinterface;
+
+public class PwidParseException extends Exception {
+
+	public PwidParseException(String string) {
+		super(string);
+	}
+
+}
--- src/main/java/dk/netarkivet/research/webinterface/PwidResolverSitesection.java
+++ src/main/java/dk/netarkivet/research/webinterface/PwidResolverSitesection.java
@@ -2,35 +2,35 @@ package dk.netarkivet.research.webinterface;
 
 import dk.netarkivet.common.webinterface.SiteSection;
 
+/**
+ * Site section that creates the menu for WPID resolution.
+ */
+public class PwidResolverSitesection extends SiteSection {
 	/**
-	 * Site section that creates the menu for WPID resolution.
+	 * The number of pages visible in the menu. The visible pages:
 	 */
-	public class PwidResolverSitesection extends SiteSection {
-	    /**
-	     * The number of pages visible in the menu. The visible pages:
-	     */
-	    private static final int PAGES_VISIBLE_IN_MENU = 1;
-	    private static final String TRANSLATIONS_BUNDLE = "wpid";
+	private static final int PAGES_VISIBLE_IN_MENU = 1;
+	private static final String TRANSLATIONS_BUNDLE = "wpid";
 
-	    /**
-	     * Create a new WpidResolverSitesection SiteSection object.
-	     */
-	    public PwidResolverSitesection() {
-	        super("mainname;pwidresolver", "PwidResolver", PAGES_VISIBLE_IN_MENU, new String[][] {
-	                {"PwpidResolver", "pagetitle;pwidresolver"},
-	                }, 
-	                "PwidResolver", TRANSLATIONS_BUNDLE);
-	    }
-	    
-	    /**
-	     */
-	    public void initialize() {
-	    }
+	/**
+	 * Create a new WpidResolverSitesection SiteSection object.
+	 */
+	public PwidResolverSitesection() {
+		super("mainname;pwidresolver", "PwidResolver", PAGES_VISIBLE_IN_MENU, new String[][] {
+				{"PwpidResolver", "pagetitle;pwidresolver"},
+		}, 
+		"PwidResolver", TRANSLATIONS_BUNDLE);
+	}
 
-	    /** No cleanup necessary in this site section. */
-	    public void close() {
-	    }
-	
+	/**
+	 */
+	public void initialize() {
+	}
+
+	/** No cleanup necessary in this site section. */
+	public void close() {
+	}
+
 	public static void main(String[] args) {
 	}
 
